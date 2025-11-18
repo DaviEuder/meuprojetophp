2c11c868f5dbb2a964fdd721bf5b7e2b86afc0d0
DaviEuder <papaleguasflexfumegante@gmail.com>
2025-11-18 09:51:04 -0300

    Update test_conn.php

--- test_conn.php
+++ test_conn.php
@@ -1,16 +1,18 @@
 <?php
+
 $host = "dpg-d4d5scal19vc73cbpd50-a.oregon-postgres.render.com";
 $db   = "meuprojetodb";
 $user = "meuprojetodb_user";
 $pass = "ARG3AoSXIauNk31ENsEeaMd4hJVZE0pz";
 
-$dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=verify-full";
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=verify-full;sslrootcert=/var/www/.postgresql/root.crt";
 
 try {
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    echo "✔ CONECTOU!";
+
+    echo "✔ CONECTOU COM SUCESSO!";
 } catch (Exception $e) {
-    echo "❌ Falhou:<br><pre>" . $e->getMessage() . "</pre>";
+    echo "❌ ERRO:<br><pre>" . $e->getMessage() . "</pre>";
 }
