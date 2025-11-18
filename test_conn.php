49a23ff5b12ddf4a139c4afa8730045e1c8f8799
DaviEuder <papaleguasflexfumegante@gmail.com>
2025-11-18 10:00:48 -0300

    Update test_conn.php

--- test_conn.php
+++ test_conn.php
@@ -1,18 +1,20 @@
 <?php
-
-$host = "dpg-d4d5scal19vc73cbpd50-a.oregon-postgres.render.com";
-$db   = "meuprojetodb";
-$user = "meuprojetodb_user";
-$pass = "ARG3AoSXIauNk31ENsEeaMd4hJVZE0pz";
-
-$dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=verify-full;sslrootcert=/var/www/.postgresql/root.crt";
-
 try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $host = getenv("PGHOST");
+    $port = getenv("PGPORT");
+    $db   = getenv("PGDATABASE");
+    $user = getenv("PGUSER");
+    $pass = getenv("PGPASSWORD");
 
-    echo "✔ CONECTOU COM SUCESSO!";
-} catch (Exception $e) {
-    echo "❌ ERRO:<br><pre>" . $e->getMessage() . "</pre>";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
+
+    $pdo = new PDO($dsn, $user, $pass, $options);
+
+    echo "✔ Conexão bem sucedida!";
+} catch (PDOException $e) {
+    echo "❌ Erro ao conectar ao banco<br>" . $e->getMessage();
 }
