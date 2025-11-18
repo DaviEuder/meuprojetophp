69589d804582175ec3a730d44eb14b73a761325d
DaviEuder <papaleguasflexfumegante@gmail.com>
2025-11-18 08:29:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,12 @@ $user = "meuprojetodb_user";
 $pass = "ARG3AoSXIauNk31ENsEeaMd4hJVZE0pz";
 $port = "5432";
 
-$connString = "host=$host port=$port dbname=$db user=$user password=$pass sslmode=require sslrootcert=system";
+$connString = "host=$host port=$port dbname=$db user=$user password=$pass sslmode=verify-full sslrootcert=system";
 $conn = pg_connect($connString);
 
 if ($conn) {
-    echo "<h1>🏀 Conexão estabelecida com PostgreSQL 18!</h1>";
-    $result = pg_query($conn, "SELECT * FROM registros_partida ORDER BY pontos DESC");
-    while ($row = pg_fetch_assoc($result)) {
-        echo "{$row['nome_jogador']} - {$row['pontos']} pontos<br>";
-    }
+    echo "<h1>🏀 Conexão estabelecida com PostgreSQL 18 (Opção A)!</h1>";
 } else {
-    echo "❌ Erro ao conectar (Opção 1).";
+    echo "❌ Erro ao conectar (Opção A).";
 }
 ?>
