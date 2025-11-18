af15b98916e20a34c89b41955608bce9df6542c1
DaviEuder <papaleguasflexfumegante@gmail.com>
2025-11-18 03:48:37 -0300

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -1,1 +1,39 @@
+<?php
+$host = "dpg-d4d5scali9vc73cbpd50-a";
+$port = "5432";
+$dbname = "meuprojetodb";
+$user = "meuprojetodb_user";
+$password = "ARG3AoSXIauNK3IEnsEeaMd4hJvZEOpsz";
+
+try {
+    $conn = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $nome = $_POST['nome'] ?? '';
+    $pontos = $_POST['pontos'] ?? 0;
+
+    if (empty($nome)) {
+        echo "Erro: nome vazio.";
+        exit;
+    }
+
+    $sql = "INSERT INTO registros_partida (nome_jogador, pontos)
+            VALUES (:nome, :pontos)";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":nome", $nome);
+    $stmt->bindParam(":pontos", $pontos, PDO::PARAM_INT);
+    $stmt->execute();
+
+    echo "OK";
+
+} catch (PDOException $e) {
+    echo "Erro ao inserir no banco: " . $e->getMessage();
+}
+
 
