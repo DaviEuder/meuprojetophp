6fbdc3ab4aee5889cbfede5f547dedf082b8d824
DaviEuder <papaleguasflexfumegante@gmail.com>
2025-11-18 09:26:26 -0300

    Create test_conn.php

---
+++ test_conn.php
@@ -0,0 +1,16 @@
+<?php
+$host = "dpg-d4d5scal19vc73cbpd50-a.oregon-postgres.render.com";
+$db   = "meuprojetodb";
+$user = "meuprojetodb_user";
+$pass = "ARG3AoSXIauNk31ENsEeaMd4hJVZE0pz";
+
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=require";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "✔ CONECTOU!";
+} catch (Exception $e) {
+    echo "❌ Falhou:<br><pre>" . $e->getMessage() . "</pre>";
+}
